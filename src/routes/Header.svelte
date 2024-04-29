6d75904c6c5f0509387e67780014a7650a279f25
sameersul <sameerjuin@gmail.com>
2024-04-29 18:58:36 -0400

    fixedheader

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,15 +4,7 @@
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -27,15 +19,8 @@
 				<a href="/contact">Contact</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 4 6" aria-hidden="true">
-		</svg>
 	</nav>
 	
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-		</a>
-	</div>
 </header>
 
 <style>
@@ -44,35 +29,10 @@
 		justify-content: space-between;
 	}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 10em;
-		height: 10em;
-		object-fit: contain;
-	}
-
 	nav {
-		display: flex;
+		flex-grow:1;
 		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
+		--background: rgb(255, 255, 255);
 	}
 
 	ul {
