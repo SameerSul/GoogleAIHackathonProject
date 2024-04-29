c694a5655e232defdae8c7b2aa123459a10f78bc
Apep <mailsafe923@gmail.com>
2024-04-30 00:36:39 +0300

    Added figma code.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,350 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Gay Porn</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<div class="div">
+	<div class="div-2">
+		<div class="div-3">
+			<img
+				loading="lazy"
+				src="https://cdn.builder.io/api/v1/image/assets/TEMP/036beeb4c65ad67b32498a114e95af14c52310169de5f436f6f3a719be733e77?"
+				class="img"
+				alt="No idea"
+			/>
+			<div class="div-4">
+				<img loading="lazy" srcset="..." class="img-2" />
+				<div class="div-5">RejuvAI - Your Personal AI Healthcare Overseer</div>
+			</div>
+			<div class="div-6">
+				<div class="div-7">
+					<div class="div-8">Home</div>
+					<div class="div-9" />
+				</div>
+				<div class="div-10">About</div>
+				<div class="div-11">FAQ</div>
+				<div class="div-12">Contact</div>
+			</div>
+		</div>
+		<div class="div-13">
+			<div class="div-14">
+				<img
+					loading="lazy"
+					src="https://cdn.builder.io/api/v1/image/assets/TEMP/0e5e2ffcd0e6bc3ddf4b7436b073bf1458b0baa6d906bfd7ca3894dd153f433e?"
+					class="img-3"
+					alt="No idea"
+				/>
+			</div>
+			<div class="div-15">
+				<div class="div-16">
+					<div class="div-17">
+						<div class="div-18" />
+						<div class="div-19" />
+					</div>
+					<div class="div-20">
+						i.e Mental Conditions, Preferred methods of treatments or more info on past conditions
+						to better optimize results
+					</div>
+				</div>
+				<div class="div-21">Enter Surgeries here</div>
+				<div class="div-22">Submit</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <style>
-	section {
+	.div {
+		box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+		border-color: rgba(37, 254, 150, 1);
+		border-style: solid;
+		border-width: 1px;
+		background-color: #fff;
 		display: flex;
 		flex-direction: column;
+		gap: 0px;
 		justify-content: center;
-		align-items: center;
-		flex: 0.6;
 	}
-
-	h1 {
+	@media (max-width: 991px) {
+		.div {
+			flex-wrap: wrap;
+		}
+	}
+	.div-2 {
+		border-color: rgba(14, 14, 14, 1);
+		border-style: solid;
+		border-width: 1px;
+		background-color: rgba(133, 235, 255, 0.29);
+		display: flex;
 		width: 100%;
+		padding-bottom: 80px;
+		flex-direction: column;
+		gap: 20px;
 	}
-
-	.welcome {
-		display: block;
+	@media (max-width: 991px) {
+		.div-2 {
+			max-width: 100%;
+			flex-wrap: wrap;
+		}
+	}
+	.div-3 {
+		disply: flex;
+		flex-direction: column;
+		fill: #000;
+		stroke-width: 1px;
+		stroke: #000;
+		overflow: hidden;
+		border-color: rgba(0, 0, 0, 1);
+		border-style: solid;
+		border-width: 1px;
 		position: relative;
+		display: flex;
+		min-height: 157px;
 		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+		gap: 20px;
+		font-size: 26px;
+		color: #bfd1e5;
+		font-weight: 700;
+		justify-content: space-between;
+		padding: 0 80px 26px;
 	}
-
-	.welcome img {
+	@media (max-width: 991px) {
+		.div-3 {
+			max-width: 100%;
+			flex-wrap: wrap;
+			padding: 0 20px;
+		}
+	}
+	.img {
 		position: absolute;
-		width: 100%;
+		inset: 0;
 		height: 100%;
-		top: 0;
-		display: block;
+		width: 100%;
+		object-fit: cover;
+		object-position: center;
+	}
+	.div-4 {
+		position: relative;
+		display: flex;
+		gap: 0px;
+	}
+	@media (max-width: 991px) {
+		.div-4 {
+			flex-wrap: wrap;
+		}
+	}
+	.img-2 {
+		aspect-ratio: 1.61;
+		object-fit: auto;
+		object-position: center;
+		width: 210px;
+		max-width: 100%;
+		gap: 0px;
+	}
+	.div-5 {
+		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		-webkit-text-stroke-width: 1;
+		-webkit-text-stroke-color: #000;
+		font-family: Georama, sans-serif;
+		border-color: rgba(0, 0, 0, 1);
+		border-style: solid;
+		border-width: 1px;
+		align-self: start;
+		margin-top: 37px;
+		gap: 0px;
+		flex-grow: 1;
+		flex-basis: auto;
+	}
+	.div-6 {
+		position: relative;
+		display: flex;
+		align-items: start;
+		gap: 20px;
+		white-space: nowrap;
+		justify-content: space-between;
+		margin: auto 0;
+	}
+	@media (max-width: 991px) {
+		.div-6 {
+			max-width: 100%;
+			flex-wrap: wrap;
+			white-space: initial;
+		}
+	}
+	.div-7 {
+		display: flex;
+		flex-direction: column;
+		gap: 6px;
+	}
+	@media (max-width: 991px) {
+		.div-7 {
+			white-space: initial;
+		}
+	}
+	.div-8 {
+		font-family: Georama, sans-serif;
+		gap: 0px;
+	}
+	.div-9 {
+		background-color: #d6d4ff;
+		margin-top: 6px;
+		height: 5px;
+		gap: 0px;
+	}
+	.div-10 {
+		font-family: Georama, sans-serif;
+		gap: 0px;
+	}
+	.div-11 {
+		font-family: Georama, sans-serif;
+		align-self: stretch;
+		gap: 0px;
+	}
+	.div-12 {
+		font-family: Georama, sans-serif;
+		gap: 0px;
+	}
+	.div-13 {
+		align-self: center;
+		display: flex;
+		gap: 20px;
+		margin: 207px 0 141px -109px;
+	}
+	@media (max-width: 991px) {
+		.div-13 {
+			flex-wrap: wrap;
+			margin: 40px 0;
+		}
+	}
+	.div-14 {
+		align-items: center;
+		align-self: start;
+		display: flex;
+		margin-top: 36px;
+		gap: 0px;
+		justify-content: center;
+		padding: 80px 0;
+	}
+	.img-3 {
+		aspect-ratio: 33.33;
+		object-fit: auto;
+		object-position: center;
+		width: 100%;
+		stroke-width: 2px;
+		stroke: #000;
+		border-color: rgba(0, 0, 0, 1);
+		border-style: solid;
+		border-width: 2px;
+		margin-top: 39px;
+		gap: 0px;
+	}
+	.div-15 {
+		display: flex;
+		flex-direction: column;
+		align-items: start;
+		gap: 20px;
+		flex-grow: 1;
+		flex-basis: 0;
+		width: fit-content;
+	}
+	@media (max-width: 991px) {
+		.div-15 {
+			max-width: 100%;
+			flex-wrap: wrap;
+		}
+	}
+	.div-16 {
+		align-self: stretch;
+		display: flex;
+		gap: 20px;
+		justify-content: space-between;
+	}
+	@media (max-width: 991px) {
+		.div-16 {
+			max-width: 100%;
+			flex-wrap: wrap;
+		}
+	}
+	.div-17 {
+		align-self: start;
+		display: flex;
+		margin-top: 11px;
+		flex-direction: column;
+		gap: 20px;
+	}
+	.div-18 {
+		border-radius: 15px;
+		background: linear-gradient(
+			90deg,
+			rgba(164, 247, 197, 0.95) 0%,
+			rgba(115, 173, 138, 0.95) 82%,
+			rgba(110, 166, 132, 0.95) 85%,
+			rgba(96, 145, 116, 0.95) 92%,
+			rgba(119, 180, 144, 0.95) 95.5%
+		);
+		backdrop-filter: blur(10px);
+		height: 50px;
+		gap: 0px;
+	}
+	.div-19 {
+		border-radius: 15px;
+		backdrop-filter: blur(10px);
+		background-color: rgba(164, 247, 197, 0.95);
+		margin-top: 70px;
+		height: 50px;
+		gap: 0px;
+	}
+	@media (max-width: 991px) {
+		.div-19 {
+			margin-top: 40px;
+		}
+	}
+	.div-20 {
+		justify-content: flex-end;
+		border-radius: 15px;
+		backdrop-filter: blur(10px);
+		background-color: rgba(164, 247, 197, 0.95);
+		color: rgba(22, 22, 30, 0.45);
+		padding: 33px 13px 80px;
+		font: 700 16px Inter, sans-serif;
+	}
+	@media (max-width: 991px) {
+		.div-20 {
+			max-width: 100%;
+			flex-wrap: wrap;
+		}
+	}
+	.div-21 {
+		align-items: start;
+		border-radius: 15px;
+		backdrop-filter: blur(10px);
+		background-color: rgba(164, 247, 197, 0.95);
+		margin-top: 66px;
+		width: 300px;
+		max-width: 100%;
+		color: rgba(22, 22, 30, 0.45);
+		justify-content: center;
+		padding: 18px 30px;
+		font: 700 16px Inter, sans-serif;
+	}
+	@media (max-width: 991px) {
+		.div-21 {
+			margin-top: 40px;
+			padding: 0 20px;
+		}
+	}
+	.div-22 {
+		border-radius: 15px;
+		background-color: rgba(214, 212, 255, 0.46);
+		width: 116px;
+		max-width: 100%;
+		align-items: start;
+		color: #000;
+		white-space: nowrap;
+		margin: 48px 0 0 18px;
+		padding: 5px 14px 13px;
+		font: 700 24px Georama, sans-serif;
+	}
+	@media (max-width: 991px) {
+		.div-22 {
+			padding-right: 20px;
+			white-space: initial;
+			margin: 40px 0 0 10px;
+		}
 	}
 </style>
